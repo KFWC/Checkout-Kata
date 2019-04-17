408b9ad5b7e59f9ad5ba743b4af0cab5d0b30c7a
Kevin Carbin <kcarbin73@gmail.com>
2019-04-17 17:56:48 +0100

    Added a few extra tests all associated with Item A

--- Checkout-Logic/Checkout.cs
+++ Checkout-Logic/Checkout.cs
@@ -8,14 +8,35 @@ namespace Checkout_Logic
 {
     public class Checkout : ICheckout
     {
+        private Dictionary<string,int> Items { get; set; }
+
+        public Checkout()
+        {
+            Items = new Dictionary<string, int>();
+        }
+
         public void Scan(string item)
         {
-            throw new NotImplementedException();
+            if (Items.ContainsKey(item))
+            {
+                Items[item] = (Items[item] + 1);
+            }
+            else
+            {
+                Items.Add(item, 1);
+            }
         }
 
         public int GetTotalPrice()
         {
-            throw new NotImplementedException();
+            int totalPrice = 0;
+
+            if (Items.Count > 0)
+            {
+                totalPrice = (Items["A"] * 50);
+            }
+
+            return totalPrice;
         }
     }
 }
--- Checkout-Tests/Tests.cs
+++ Checkout-Tests/Tests.cs
@@ -8,11 +8,53 @@ namespace Checkout_Tests
     public class Tests
     {
         [TestMethod]
-        public void TestNoItemsBrought()
+        public void NoItemsBrought()
         {
             Checkout checkout = new Checkout();
 
             int expected = 0;
+
+            int actual = checkout.GetTotalPrice();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ScannedOneItemA()
+        {
+            Checkout checkout = new Checkout();
+            checkout.Scan("A");
+
+            int expected = 50;
+
+            int actual = checkout.GetTotalPrice();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ScannedTwoItemsA()
+        {
+            Checkout checkout = new Checkout();
+            checkout.Scan("A");
+            checkout.Scan("A");
+
+            int expected = 100;
+
+            int actual = checkout.GetTotalPrice();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ScannedThreeItemsA()
+        {
+            Checkout checkout = new Checkout();
+            checkout.Scan("A");
+            checkout.Scan("A");
+            checkout.Scan("A");
+
+            int expected = 130;
 
             int actual = checkout.GetTotalPrice();
 
